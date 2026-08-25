063007d935f40c603c1c8f3d346601371d314e17
nagarajhegde53 <devilophunter00@gmail.com>
2026-08-25 09:34:31 +0530

    fixed6

--- notes-frontend/js/auth.js
+++ notes-frontend/js/auth.js
@@ -88,7 +88,7 @@ loginForm.addEventListener("submit", async function (event) {
     };
 
     try {
- const response = await fetch("https://run-fitness-app.onrender.com",
+ const response = await fetch("https://run-fitness-app.onrender.com/login/",
     //    const response = await fetch("http://127.0.0.1:8000/login/", 
     {
 
@@ -163,8 +163,9 @@ registerForm.addEventListener("submit", async function (event) {
     };
 
     try {
-
-        const response = await fetch("http://127.0.0.1:8000/register/", {
+const response =await fetch("https://run-fitness-app.onrender.com/register/",
+        // const response = await fetch("http://127.0.0.1:8000/register/",
+         {
 
             method: "POST",
 
