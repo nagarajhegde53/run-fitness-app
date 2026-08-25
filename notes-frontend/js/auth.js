85b945ff73af369f41970006db866e98e5ee4234
nagarajhegde53 <devilophunter00@gmail.com>
2026-08-25 08:45:13 +0530

    fixed4

--- notes-frontend/js/auth.js
+++ notes-frontend/js/auth.js
@@ -88,7 +88,7 @@ loginForm.addEventListener("submit", async function (event) {
     };
 
     try {
- const response = await fetch("https://onrender.com",
+ const response = await fetch("https://run-fitness-app.onrender.com",
     //    const response = await fetch("http://127.0.0.1:8000/login/", 
     {
 
